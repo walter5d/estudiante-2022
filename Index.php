056af37d386203e06a87cf51d31c338000d9f72a
cv-walter <wajbalo@miumg.edu.gt>
2022-09-11 23:32:47 -0600

    se agrega el proyecto

---
+++ Index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Inicio </title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+</head>
+
+<body>
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Inicio</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link active" href="#">nosostros</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link active" href="#">contactos</a>
+      </li>
+
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" href="#">Sub_Menu</a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Inicio</a></li>
+        <li><a class="dropdown-item" href="#">Registro de alumno</a></li>
+        <li><a class="dropdown-item" href="#">Gracias</a></li>
+      </ul>
+    </li> 
+    
+    <li class="nav-item ">
+        <a class="nav-link active ml-auto" href="#">contactos</a>
+      </li>
+
+     
+      
+
+  
+    
+
+   
+
+  </div>
+</nav>
+
+  
+
+
+  
+
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
+    integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+    integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
